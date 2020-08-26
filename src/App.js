4bca120e781991e6d288b2ece63898e923b991aa
QED0711 <qdizon@gmail.com>
2020-08-26 19:38:45 -0400

    testing redirect in base route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
 import SessionWindow from './components/SessionWindow';
 import ConnectionManager from './components/ConnectionManager';
 import ConfigPanel from './components/ConfigPanel';
@@ -15,8 +15,9 @@ function App() {
             <div className="App">
                 <Switch>
                     <Route exact path="/">
-                        LOGIN!!!
-                        <Link to="/session/123">LINK</Link>
+                        LOGIN!
+                        <Redirect to={"/session/umd"} />
+                        {/* <Link to="/session/123">LINK</Link> */}
                     </Route>
 
                     <Route exact path="/session/:sessionID">
