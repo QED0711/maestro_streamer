f958145ca2f315037f7797feff15b8d651d30e26
QED0711 <qdizon@gmail.com>
2020-08-26 18:50:30 -0400

    added 404.html to page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import SessionWindow from './components/SessionWindow';
 import ConnectionManager from './components/ConnectionManager';
 import ConfigPanel from './components/ConfigPanel';
@@ -16,6 +16,7 @@ function App() {
                 <Switch>
                     <Route exact path="/">
                         LOGIN FORM
+                        <Link to="/session/123">LINK</Link>
                     </Route>
 
                     <Route exact path="/session/:sessionID">
