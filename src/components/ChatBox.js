5ba6aaacc308ec7a5bd12c25fa39719322a37c51
QED0711 <qdizon@gmail.com>
2020-09-05 07:39:58 -0400

    added check for production environment when linking chat to new window

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -117,8 +117,9 @@ const ChatBox = () => {
                 // if we're not already in the chat window
                 !(/\/chat\//.test(window.location.href))
                 &&
-                <a href={`/chat/${sessionID}?name=${queryParams.name}`} target="_blank">Open Chat in new Window</a>
+                <a href={`${process.env.NODE_ENV === "production" ? "/maestro_streamer" : ""}/chat/${sessionID}?name=${queryParams.name || "anonymous"}`} target="_blank">Open Chat in new Window</a>
             }
+            {console.log(process.env.NODE_ENV)}
         </div>
     )
 
